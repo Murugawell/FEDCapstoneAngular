d67a0e87eb466bd3838f527f7bf94ca2f0b20bde
Soundar R (Digital) <SO351975@wipro.com>
2019-09-18 12:56:58 +0530

    charts and card design changes

--- src/app/components/barchart/barchart.component.ts
+++ src/app/components/barchart/barchart.component.ts
@@ -25,10 +25,15 @@ export class BarchartComponent implements OnInit {
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
-
+  product=[];
   public barChartData: ChartDataSets[] = [
     { data: [], label: 'Series A' }
   ];
+   public chartColors: Array<any> = [
+    { // first color
+      backgroundColor: '#b7bfe8',
+     
+    }];
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
@@ -37,7 +42,7 @@ export class BarchartComponent implements OnInit {
    getProduct() {
     this._productService.getProducts().subscribe(
       (product: any) => {
-        
+        this.product=product;
         var array = product;
         array.sort(function (a, b) {
           return b.views - a.views;
@@ -65,9 +70,25 @@ export class BarchartComponent implements OnInit {
     console.log(event, active);
   }
 
-  public randomize(): void {
+  public randomize(event, num): void {
     // Only Change 3 values
-    const data =this.barChartData[0].data.splice(0,3);
+    console.log(num);
+    this.barChartLabels=[];
+    this.barChartData[0].data=[];
+      for(let p in this.product)
+   {
+     console.log(typeof(this.barChartLabels));
+    this.barChartLabels.push(this.product[p].productName);
+    this.barChartData[0].data.push(this.product[p].views);
+    
+   }
+   if(num>0)
+   {
+    const data =this.barChartData[0].data.splice(0,num);
+
+    const labels= this.barChartLabels.splice(0,num);
     this.barChartData[0].data = data;
+    this.barChartLabels=labels;
+    }
   }
 }
